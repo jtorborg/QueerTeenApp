130ec40303d4690cecac38f05fc738e1302035a9
Ian Coldwater <ian@coldwater.io>
2017-01-15 09:01:00 -0600

    committing twilio setup

--- server/app.js
+++ server/app.js
@@ -17,6 +17,15 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var AccessToken = require('twilio').AccessToken;
+var IpMessagingGrant = AccessToken.IpMessagingGrant;
+
+// just twilio things
+var accountSid = 'ACbb77777fcddd099e0e68163a767c22b0';
+var authToken = "afb276bdebc44e26159780283a514850";
+var chatSid = 'IS18dcc9ff703341bf86b7f9f3e185f384';
+var apiKey = 'SKdd87da444537867ceb84e82d2d39300f';
+var apiSecret = '8b4iVlshPvsH8ctS4mqcjS2tBnVao7oa';
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -33,6 +42,44 @@ console.log(testModule.test("app.js: testing module connection"));
 //routes
 var testRoute = require("./routes/testRoute.js");
 app.use('/testRoute', testRoute);
+
+
+/*
+Generate an Access Token for a chat application user - it generates a random
+username for the client requesting a token, and takes a device ID as a query
+parameter.
+*/
+app.get('/token', function(request, response) {
+    var appName = 'TwilioChatDemo';
+    var identity = 'Anonymous Youth';
+    var deviceId = request.query.device;
+
+    // Create a unique ID for the client on their current device
+    var endpointId = appName + ':' + identity + ':' + deviceId;
+
+    // Create a "grant" which enables a client to use IPM as a given user,
+    // on a given device
+    var ipmGrant = new IpMessagingGrant({
+        serviceSid: chatSid,
+        endpointId: endpointId
+    });
+
+    // Create an access token which we will sign and return to the client,
+    // containing the grant we just created
+    var token = new AccessToken(
+        accountSid,
+        apiKey,
+        apiSecret
+    );
+    token.addGrant(ipmGrant);
+    token.identity = identity;
+
+    // Serialize the token to a JWT string and include it in a JSON response
+    response.send({
+        identity: identity,
+        token: token.toJwt()
+    });
+});
 
 
 app.get('/*', function(req, res) {
