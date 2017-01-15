ede84cee37f7b35ce9d8a70355bbb7db7dd9a522
kerij <keriajackson@gmail.com>
2017-01-14 20:03:39 -0600

    working on geo apis

--- server/public/scripts/main.controller.js
+++ server/public/scripts/main.controller.js
@@ -7,8 +7,25 @@ function mainController(mainService) {
     main.Object = {};
     main.Array = [];
 
+    //AIzaSyCAbb-bfinu1-VIj8Dgt-vAi7HMAbAPb8o
+
+    var findZips = function(lat, lng){
+      var key = 'AIzaSyCAbb-bfinu1-VIj8Dgt-vAi7HMAbAPb8o';
+      var baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
+      var request = baseURL + lat + ',' + lng + '&key=' + key
+
+      $http.get(request).then(function(response){
+        console.log(response.data);
+      }, err)
+    }
 
 
+    $scope.startChat = function() {
+      console.log("chat button clicked");
+      navigator.geolocation.getCurrentPosition(function(position){
+        findZips(position.coords.latitude, position.coords.longitude);
+      });
+    }
 
         main.getmain = function() {
             console.log('inside getmain');
