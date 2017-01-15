3b0ce5cee38c796b55bc664f40798382ab6406a9
twinklefingers <rachelweila@gmail.com>
2017-01-14 18:29:59 -0600

    took out locationProvider

--- server/public/scripts/app.config.js
+++ server/public/scripts/app.config.js
@@ -1,16 +1,16 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
 // routing
-myApp.config(function($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
+myApp.config(['$routeProvider', function($routeProvider) {
+    // $locationProvider.html5Mode(true);
 
     $routeProvider.when('/welcome', {
-        templateUrl: '../public/views/partials/welcome.html'
-        // controller: 'LoginController as login'
+        templateUrl: '../views/partials/welcome.html',
+        controller: ''
         // }).when('/logout', {
         //   templateUrl: 'views/logout.html',
         //   controller: 'LogoutController as logout'
     }).otherwise({
         redirectTo: "/welcome"
     });
-});
+}]); // end .config
